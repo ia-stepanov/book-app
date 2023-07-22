ac2f3d3f823eac27c2e046722c92c19996204c51
Igor Stepanov <igorstepanov@hotmail.com>
2023-07-22 21:47:37 +0300

    build(Book App): реализовать отрисовку страниц и роутинг

--- src/app.js
+++ src/app.js
@@ -1,2 +1,35 @@
-'use strict';
-import './app.css';
+// Импортируем компонент
+import { MainView } from './views/main/main';
+
+// Основной класс приложения
+class App {
+  // Массив маршрутов
+  routes = [{ path: '', view: MainView }];
+
+  constructor() {
+    // Подписываемся на событие изменения URL-адреса
+    window.addEventListener('hashchange', this.route.bind(this));
+
+    // Вызываем маршрутизацию
+    this.route();
+  }
+
+  // Метод для обработки маршрутов
+  route() {
+    // Если страница уже отрисована, вызываем метод destroy
+    if (this.currentView) {
+      this.currentView.destroy();
+    }
+    // Находим маршрут в массиве маршрутов по URL-адресу
+    const view = this.routes.find((r) => r.path === location.hash).view;
+
+    // Создаём экземпляр класса для отрисовки страницы
+    this.currentView = new view();
+
+    // Вызываем render, чтобы отрисовать страницу
+    this.currentView.render();
+  }
+}
+
+// Запускаем наше приложение
+new App();
